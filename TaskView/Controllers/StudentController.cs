ffff9c702d67df5da8235175a32ad47b28c5b255
Aytajrajab <aytajir@code.edu.az>
2021-12-01 04:18:23 +0400

    initial commit

---
+++ TaskView/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskView.Controllers
+{
+    public class StudentController :Controller
+    {
+        public IActionResult Index(int ?id)
+        {
+            if (id==null)
+            {
+                ViewData["Message"] = "Id is null";
+                return View("Index");
+            }
+            return Json(new {Name="Aytac",Surname="Racabli",Age=21});
+        }
+    }
+}
